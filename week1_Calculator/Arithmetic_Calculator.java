5068dc558a0b192e7db7f2affbc76b4b2fe9a596
Arvind <arvind.n228@gmail.com>
2022-07-23 14:55:41 +0530

    Second commit

--- week1_Calculator/Arithmetic_Calculator.java
+++ week1_Calculator/Arithmetic_Calculator.java
@@ -1,5 +1,5 @@
 package week1_Calculator;
-import java.util.Scanner;
+import java.util.*;
 
 class Arithmetic{
 	int variable1;
@@ -33,10 +33,10 @@ class Arithmetic{
 class ArithmeticAlso extends Arithmetic{} //using access specifier
 
 public class Arithmetic_Calculator {
-	final static double Pi=3.14; //final keyword ->constant value
-
+	
 	public static void main(String[] args) {
 		double num1, num2; //variable, double data type
+		char operator;
 		Arithmetic calculate= new Arithmetic(); //creating object calculate
 		Arithmetic temp= new Arithmetic();
 		ArithmeticAlso calculate_temp= new ArithmeticAlso();
@@ -48,13 +48,22 @@ public class Arithmetic_Calculator {
 		System.out.println("Enter second number?");
 		num2=sc.nextDouble();
 		
-		System.out.println("Arithmetic operation on the two numbers are:");
-		System.out.println("Addition is: "+ calculate.add(num1,num2));
-		System.out.println("Subtraction is: "+ calculate_temp.sub(num1, num2));//access specifier
-		System.out.println("Multiplication is: "+ calculate.multiply(num1, num2) );
-		System.out.println("Division is: "+ calculate.division(num1, num2));
-		System.out.println("First number multiplied with PI is: "+ num1*Pi+"\n");//using final keyword
-		
+		System.out.print("\nEnter an operator (+, -, *, /): \n");
+	    operator = sc.next().charAt(0);
+	    
+	    switch(operator) {
+	       case '+': System.out.println("Addition is: "+ calculate.add(num1,num2)+"\n");
+	          break;
+	       case '-': System.out.println("Subtraction is: "+ calculate_temp.sub(num1, num2)+"\n");//access specifier
+	          break;
+	       case '*': System.out.println("Multiplication is: "+ calculate.multiply(num1, num2)+"\n" );
+	          break;
+	       case '/': System.out.println("Division is: "+ calculate.division(num1, num2)+"\n");
+	          break;
+	    default: System.out.printf("Choose from given operators");
+	       return;
+	    }
+	    System.out.println();
 		Arithmetic.role="Performs 4 operations"; //static keyword, String 'role' gets changed
 		System.out.println("What does this program do?: ");
 		temp.display(); 
